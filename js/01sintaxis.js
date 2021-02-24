8e9ceb7b5dbb80794cad67a8a85433e8a35c32b3
MisaelGC <gacrmu@hotmail.com>
2021-02-24 02:35:45 -0600

    Sintaxis - Hola Mundo

---
+++ js/01sintaxis.js
@@ -0,0 +1,5 @@
+// Sintaxis inicial de JS, esta es la forma en la que se realiza un "Hola Mundo".
+// A contario de otros lenjuages, este se muestra en navegador Web
+// para verlo se abre el navegador y se presiona F12 y selecciona Console
+// Ahí podremos ver cada uno de los console, logs y errores de Sintaxis
+console.log("Hola Mundo")
