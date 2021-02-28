bdba0cc07089a2dc3ae620f1ef965d41372c28fe
MisaelGC <gacrmu@hotmail.com>
2021-02-28 02:08:09 -0600

    Concatenar

---
+++ js/09concatenar-valores.js
@@ -0,0 +1,5 @@
+/* Concatenar: Es la acción de mezclar variables con cadenas de texto y se utilizan el caracter + para separar
+el texto de la variables */
+var nombre = "Misael";
+var edad = 26;
+console.log("Su nombre es " + nombre + " y tiene " + edad + " años")
